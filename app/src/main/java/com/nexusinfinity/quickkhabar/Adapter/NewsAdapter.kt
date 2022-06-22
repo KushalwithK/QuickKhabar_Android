bbec8695362cec71837bd229aee09d511a0a74d7
KushalwithK <kushalshah821@gmail.com>
2022-06-22 17:34:28 +0530

    Made recycler view but giving error (no adapter connected, skipping content)

---
+++ app/src/main/java/com/nexusinfinity/quickkhabar/Adapter/NewsAdapter.kt
@@ -0,0 +1,30 @@
+package com.nexusinfinity.quickkhabar.Adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.nexusinfinity.quickkhabar.R
+
+class NewsAdapter(var news: ArrayList<String>) : RecyclerView.Adapter<NewsViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.news_view, parent, false);
+        return NewsViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        val currentItem = news[position]
+        holder.titleView.text = currentItem
+    }
+
+    override fun getItemCount(): Int {
+        return news.size
+    }
+}
+
+class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    val titleView : TextView = itemView.findViewById(R.id.titleView)
+}
--- app/src/main/java/com/nexusinfinity/quickkhabar/MainActivity.kt
+++ app/src/main/java/com/nexusinfinity/quickkhabar/MainActivity.kt
@@ -2,10 +2,32 @@ package com.nexusinfinity.quickkhabar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.nexusinfinity.quickkhabar.Adapter.NewsAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    private var recyclerView : RecyclerView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView?.layoutManager = LinearLayoutManager(this)
+        recyclerView?.setHasFixedSize(true)
+
+        val getItems = fetchData()
+        val adapter = NewsAdapter(getItems)
+        recyclerView?.adapter = adapter;
+    }
+
+    private fun fetchData() : ArrayList<String> {
+        val list = ArrayList<String>()
+        for (i in 0 until 50){
+            list.add("Item $i")
+        }
+
+        return list
     }
 }
